708c3d61babd11c235dec267df5880dee3966ac9
Jessica Miller <jessicamiller@Jessicas-MacBook-Pro.local>
2020-10-03 13:51:13 -0700

    Incomplete Code - Only JavaDoc Added

---
+++ Documents/CSE-360/Assignment1/src/cse360assignment02/AddingMachineOriginal.java
@@ -0,0 +1,84 @@
+/** <h1>Author Information</h1>
+ *  Name: Jessica Miller
+ *  ASU ID: 1214708170
+ *  Email: jtmill25@asu.edu
+ *  Date Edited: 10/03/2020
+ */
+/** <h1>Program Description</h1>
+ *  This program has little functionality at this point, all that is happening is an
+ *  addition of comments and practice with version control.
+ */
+
+package cse360assignment02; //inherits from package cse360assignment02
+
+/**
+ * This public class is being called AddingMachine
+ */
+public class AddingMachine
+{
+    /**
+     * Variables declared
+     */
+    private int total;
+   
+
+    /**
+     * calling method AddingMachine
+     * using to set total to 0
+     */
+    public AddingMachine ()
+    {
+        total = 0;
+       
+    }
+
+    /**
+     * calling method getTotal
+     * returns current value of var total
+     */
+
+    public int getTotal ()
+    {
+        return 0;
+    }
+
+    /**
+     * calling method add
+     */
+
+    public void add (int value)
+    {
+      
+    }
+
+    /**
+     * calling method subtract
+     */
+
+    public void subtract (int value)
+    {
+        
+    }
+
+    /**
+     * calling method String as a toString method
+     */
+
+    public String toString ()
+    {
+        return " ";
+    }
+
+    /**
+     * calling method clear
+     * no content at the moment
+     */
+
+    public void clear()
+    {
+    }
+}
+
+/** <h1>Resources</h1>
+ * https://idratherbewriting.com/java-share-files/
+ */
