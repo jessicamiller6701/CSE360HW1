4d23b0a5bc2ed1e37a53f54c2c7770b90a5da001
Jessica Miller <jessicamiller@Jessicas-MacBook-Pro.local>
2020-10-03 12:00:38 -0700

    package cse360assignment02 contains addingmachine.java

---
+++ Documents/CSE-360/Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,49 @@
+/** <h1>Author Information</h1>
+ *  Name: Jessica Miller
+ *  ASU ID: 1214708170
+ *  Email: jtmill25@asu.edu
+ *  Date Edited: 10/03/2020
+ */
+/** <h1>Program Description</h1>
+ *  This program has little functionality at this point, all that is happening is an
+ *  addition of comments and practice with version control.
+ */
+
+package cse360assignment02; //inherits from package cse360assignment02
+
+public class AddingMachine //calling class AddingMachine
+{
+    private int total; //declaring private integer called total
+
+    public AddingMachine ()  //method called AddingMachine
+    {
+        total = 0;  // not needed - included for clarity
+    }
+
+    public int getTotal ()  //method called getTotal
+    {
+        return 0; //return getTotal
+    }
+
+    public void add (int value)  //void method called add
+    {
+    }
+
+    public void subtract (int value)  //void method called subtract
+    {
+    }
+
+    public String toString ()  //toString method called String
+    {
+        return ""; //return a string
+    }
+
+    public void clear()  //void method called clear
+    {
+    }
+}
+
+/** <h1>Resources</h1>
+ * https://idratherbewriting.com/java-share-files/
+ *
+ */
